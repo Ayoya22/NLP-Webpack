361d635a0a1a6aa45fd33f5d66c0ad9095b80a19
ayoya22 <djoukamsimplice@gmail.com>
2019-08-26 16:38:42 +0100

    removed and unnecessary tests files and included some new tests to volidate my API's.
    Signed-off-by: ayoya22 <djoukamsimplice@gmail.com>

--- jest_tests/NameCheckerTest.test.js
+++
@@ -1,7 +0,0 @@
-//import {checkForName} from "../src/client";
-
-const x = require('./nameChecker');
-
-test('check if the name is a valid captain name', () => {
-    expect(x).tobe(120);
-});
--- src/server/index.js
+++ src/server/index.js
@@ -57,6 +57,7 @@ app.post('/article', function(req, res) {
     });
 })
 
+export {app}
 // designates what port the app will listen to for incoming requests
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!')
